227dcc34e6359081a7af1cd8300be71bc414e9a7
apexsolutions <imperialkatelyn@gmail.com>
2026-06-02 13:35:30 +0700

    Update app.js

--- app.js
+++ app.js
@@ -51,14 +51,6 @@ document.querySelectorAll('a[target="_blank"]').forEach((link) => {
   });
 });
 
-
-document.querySelectorAll('a[target="_blank"]').forEach((link) => {
-  link.addEventListener("click", function (e) {
-    e.stopPropagation();
-    window.open(this.href, "_blank", "noopener,noreferrer");
-  });
-});
-
 document.querySelectorAll('a[href^="#"]').forEach(link => {
   link.addEventListener("click", function (e) {
     const targetId = this.getAttribute("href");
